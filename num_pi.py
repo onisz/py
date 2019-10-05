7c4311a09237b537f37626f359d495a0f967681b
Andrey Kirsanov <39jvQV81@inbox.ru>
2019-10-05 22:08:57 +0300

    2

--- num_pi.py
+++
@@ -1,6 +0,0 @@
-import math
-def func(x):
-   return f'{math.pi:.{a}}'
-a = int(input())
-print(func(a))
-    
--- str_n.py
+++
@@ -1,8 +0,0 @@
-def func(x, y):
-    if len(s) > n:
-        return s.upper()
-    else:
-        return s
-s = str(input("Введите слово: "))
-n = int(input("Введите число: "))
-print(func(s, n))
